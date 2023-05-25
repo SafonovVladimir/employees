e47276a5db365c4cf3bf2199245b643b6d755f68
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-05-25 18:01:44 +0300

    Add models

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+    date_hired = models.DateField()
+    email = models.EmailField()
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    manager = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="employees"
+    )
+
+    def __str__(self):
+        return self.full_name
