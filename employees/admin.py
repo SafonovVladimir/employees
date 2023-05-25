3139090224a7ad0e15007cda1732d314d31a194c
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-05-25 18:02:37 +0300

    Register EmployeeModel in admin.py

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_title = "Manage employees"
+admin.site.site_header = "Manage employees"
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("id", "full_name", "date_hired", "email", "position")
+    list_display_links = ("id", "full_name")
+    search_fields = ("full_name", "position", "email")
+    save_on_top = True
