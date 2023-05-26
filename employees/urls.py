e8f6a9061871fe2f017ecedbc4f5fc5f4dc81b16
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-05-26 07:17:59 +0300

    Added auth with validation to urls.py

--- employees/urls.py
+++ employees/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 
-from employees.views import index
+from employees.views import (
+    index,
+    user_login,
+    user_logout,
+    register,
+    validate_username,
+    check_username
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("register/", register, name="register"),
+    path("validate_username/", validate_username, name="validate_username"),
+    path("check_username/", check_username, name="check_username"),
 ]
 
 app_name = "employees"
--- employees/views.py
+++ employees/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+
+from employees.forms import UserLoginForm, UserRegisterForm
+
 
 def index(request):
     """View function for the home page of the site."""
@@ -11,3 +18,57 @@ def index(request):
 
     return render(request, "employees/index.html", context=context)
 
+
+def user_login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("employees:index")
+    else:
+        form = UserLoginForm()
+    return render(request, "employees/login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("employees:login")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        context = {
+            "form": form,
+        }
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("employees:index")
+        else:
+            messages.error(request, "Error")
+    else:
+        form = UserRegisterForm()
+        context = {
+            "form": form,
+        }
+    return render(request, "employees/register.html", context=context)
+
+
+def validate_username(request):
+    """Check available name"""
+    username = request.GET.get("username", None)
+    response = {
+        "is_taken": User.objects.filter(username__iexact=username).exists()
+    }
+    return JsonResponse(response)
+
+
+def check_username(request):
+    """Check name"""
+    username = request.GET.get("username", None)
+    response = {
+        "is_taken": User.objects.filter(username__iexact=username).exists()
+    }
+    return JsonResponse(response)
