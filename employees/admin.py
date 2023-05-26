92c6c30ec39207e611a3c56b26567bf9a310abc2
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-05-26 07:16:33 +0300

    Registered Employee and Position in admin.py

--- employees/admin.py
+++ employees/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 
+from employees.models import Employee, Position
+
 admin.site.site_title = "Manage employees"
 admin.site.site_header = "Manage employees"
 
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ("id", "full_name", "date_hired", "email", "position")
+    list_display = (
+        "id",
+        "full_name",
+        "date_hired",
+        "email",
+        "position",
+        "manager"
+    )
     list_display_links = ("id", "full_name")
     search_fields = ("full_name", "position", "email")
     save_on_top = True
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Position)
